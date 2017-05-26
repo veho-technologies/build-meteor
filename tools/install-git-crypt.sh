12257ab8f3df6da0bef6947eee9d66307c6f1720
Daniel Holzmann <d@velopment.at>
2017-05-26 12:57:23 +0200

    use git-crypt 0.5.0

--- tools/install-git-crypt.sh
+++ tools/install-git-crypt.sh
@@ -6,10 +6,12 @@ apt-get update \
     && apt-get install -y \
     libssl-dev g++ openssl
 
-wget https://github.com/AGWA/git-crypt/archive/debian/0.5.0-2.tar.gz
-tar -xvzf 0.5.0-2.tar.gz
-rm -f 0.5.0-2.tar.gz
+wget https://github.com/AGWA/git-crypt/archive/0.5.0.tar.gz
+tar -xvzf 0.5.0.tar.gz
+rm -f 0.5.0.tar.gz
 
-cd git-crypt-debian-0.5.0-2
+cd git-crypt-0.5.0
 make
 make install PREFIX=/usr/local
+cd ..
+rm -Rf git-crypt-0.5.0
